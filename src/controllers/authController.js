8003ef68b608c4f1a3b0786441f0596e0f4a3879
gustavo.silva <gustavo.silva@geofusion.com.br>
2020-05-08 15:34:59 -0300

    create authenticator and project route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user');
 
 const router = express.Router();
+
+function generateToken(params = {}){
+  return jwt.sign(params, authConfig.secret, { expiresIn: 86400 });
+}
 
 router.post('/register', async(req, res) => {
   const { email } = req.body;
@@ -15,7 +22,7 @@ router.post('/register', async(req, res) => {
 
     const user = await User.create(req.body);
     user.password = undefined;
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id })});
 
   } catch (error) {
     return res.status(400).send({ erro: 'Registration Failed' });
@@ -37,7 +44,7 @@ router.post('/authenticate', async(req,res) => {
 
   user.password = undefined;
 
-  res.send({ user });
+  res.send({ user, token: generateToken({ id: user.id }) });
 })
 
 module.exports = app => app.use('/auth', router);
